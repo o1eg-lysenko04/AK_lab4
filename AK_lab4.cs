2f51948208dc84d93ffd84decfa04bf00cff7c3b
o1eg-lysenko04 <133224448+o1eg-lysenko04@users.noreply.github.com>
2023-05-22 17:14:07 +0300

    Lab4_AK

---
+++ AK_lab4.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string directoryPath = "путь_к_каталогу";
+
+        int fileCount = CountFilesInDirectory(directoryPath);
+
+        Console.WriteLine("Количество файлов в каталоге: " + fileCount);
+    }
+
+    static int CountFilesInDirectory(string directoryPath)
+    {
+        int count = 0;
+
+        // Получаем список файлов в текущем каталоге
+        string[] files = Directory.GetFiles(directoryPath);
+
+        // Увеличиваем счетчик на количество файлов в текущем каталоге
+        count += files.Length;
+
+        // Рекурсивно обходим все подкаталоги
+        string[] subdirectories = Directory.GetDirectories(directoryPath);
+        foreach (string subdirectory in subdirectories)
+        {
+            count += CountFilesInDirectory(subdirectory);
+        }
+
+        return count;
+    }
+}
